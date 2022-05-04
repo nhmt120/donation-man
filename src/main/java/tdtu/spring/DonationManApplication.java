0249a498bf56eeb22cb0cdd7df693b3d7a4108da
vincemarcus <vincemarcus1209@gmail.com>
2022-05-04 16:30:07 +0700

    Initial commit

---
+++ src/main/java/tdtu/spring/DonationManApplication.java
@@ -0,0 +1,13 @@
+package tdtu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationManApplication.class, args);
+	}
+
+}
